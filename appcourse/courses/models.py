214e5cc37070153a1fa3343a800d44a112ab03bc
Antonio Ricardo <antonio.eschola@gmail.com>
2017-08-20 21:41:22 -0400

    Alter field image on model course to receiver blank data and null data

--- appcourse/courses/models.py
+++ appcourse/courses/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class Course(models.Model):
+    # definições do modelo
     title = models.CharField(blank=True, max_length=100, verbose_name='Título')
     slug = models.SlugField(verbose_name='Atalho')
     description = models.TextField(blank=True, verbose_name='Descrição')
     start_date = models.DateField(null=True, blank=True, verbose_name='Data de Início')
-    image = models.ImageField(upload_to='courses/images', verbose_name='Imagem')
+    image = models.ImageField(upload_to='courses/images',
+                              null=True, blank=True, verbose_name='Imagem')
     created_at = models.DateTimeField(verbose_name='Criado em', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='Atualizado em', auto_now=True)
