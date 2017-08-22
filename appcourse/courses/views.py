57e0d03c219697b36f9fd4d85de44fe8262df76b
Antonio Ricardo <antonio.eschola@gmail.com>
2017-08-22 10:46:22 -0400

    Adjusting form contact to course

--- appcourse/courses/views.py
+++ appcourse/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Course
+from .forms import ContactCourse
 
 def courses(request):
     courses = Course.objects.all()
@@ -20,8 +21,10 @@ def course_detail(request, pk):
 
 def course_detail(request, slug):
     course = get_object_or_404(Course, slug=slug)
+    form = ContactCourse()
     context = {
         'course': course,
+        'form': form,
     }
     template_name = 'courses/course_detail.html'
     return render(request, template_name, context)
